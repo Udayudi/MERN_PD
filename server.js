2947245d341a83e6fc7225952d550bd9d190baf6
Udayudi <udaygam@outlook.com>
2020-10-27 18:40:47 +0530

    added react client

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// const connectDB = require("./config/db");
+
+// // DB Connection
+// connectDB();
+
+// Init Meddleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("API is Working");
+});
+
+// Routes
+// app.use("/api/personalDetails", require("./routes/api/personalDetails"));
+
+const port = process.env.PORT || 8001;
+app.listen(port, () => `Server running on port ${port}`);
